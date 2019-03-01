eafa3a10dee748a0673d6f3955a54d71da9e6d57
hq1220 <48111723+hq1220@users.noreply.github.com>
2019-03-01 20:05:47 +0800

    Update test_network.py
    删除

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -36,4 +36,3 @@ class TestNetwork:
         self.network_page.click_more()
         self.network_page.click_network()
         self.network_page.click_first_network()
-        self.network_page.click_5g()
